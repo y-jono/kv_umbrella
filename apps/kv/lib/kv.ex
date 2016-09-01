ae61b5709c133d2a0a0d3f244cfd3f91f77701a2
Yoshinori SEINO <y_seino@dynasystem.co.jp>
2016-09-01 13:03:19 +0900

    initial commit

---
+++ apps/kv/lib/kv.ex
@@ -0,0 +1,21 @@
+defmodule KV do
+  use Application
+
+  def start(_type, _args) do
+    KV.Supervisor.start_link
+  end
+
+  #def start_link do
+  #  Task.start_link(fn -> loop(%{}) end)
+  #end
+
+  #defp loop(map) do
+  #  receive do
+  #    {:get, key, caller} ->
+  #      send caller, Map.get(map, key)
+  #      loop(map)
+  #    {:put, key, value} ->
+  #      loop(Map.put(map, key, value))
+  #  end
+  #end
+end
